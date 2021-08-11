b493e8f072b4769a7c56a36f4cf7401c12c69ff4
zhengjie <601116847@qq.com>
2021-08-11 17:51:56 +0800

    排序

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -1,11 +1,9 @@
 package sort;
 
-import static java.util.Arrays.sort;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int[] nums = {83, 74, 12, 16, 35, 46, 36, 23, 41, 75};
-        bubbleSort(nums);
+        insertSort(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }
@@ -15,19 +13,66 @@ public class Main {
     /**
      * 冒泡排序
      *
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
+
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                break;
+            }
+        }
+
+    }
+
+    /**
+     * 选择排序
+     *
      * @param nums
      */
-    public static void bubbleSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j + 1];
-                    nums[j + 1] = nums[j];
-                    nums[j] = temp;
+    public static void selectSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
                 }
+            }
+            if (minIndex != i) {
+                swap(nums, minIndex, i);
+
             }
         }
     }
+
+    /**
+     * 插入排序
+     *
+     * @param nums
+     */
+    public static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int j = i;
+            int tmp = nums[j];
+            for (; j > 0 && tmp < nums[j - 1]; j--) {
+                nums[j] = nums[j - 1];
+            }
+            if (j != i) {
+                nums[j] = tmp;
+            }
+        }
+    }
+
 
     public static void mergerSort(int[] nums) {
         merge(nums, 0, nums.length);
@@ -54,5 +99,11 @@ public class Main {
 
             }
         }
+    }
+
+    private static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
