f05d8f011ea6e0e9dddc91476c541b922315e612
601116847@qq.com <601116847@qq.com>
2022-07-26 16:22:16 +0800

    stack 2

---
+++ leetcode/editor/cn/[剑指 Offer 06]从尾到头打印链表.java
@@ -0,0 +1,45 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+// Related Topics 栈 递归 链表 双指针 
+// 👍 320 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public int[] reversePrint(ListNode head) {
+        ListNode p = head;
+        Stack<Integer> s = new Stack<>();
+        while (p!=null){
+            s.push(p.val);
+            p = p.next;
+        }
+        int l = s.size();
+        int[]a =new int[l];
+        for (int i = 0; i < l; i++) {
+            a[i]=s.pop();
+        }
+        return a;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
